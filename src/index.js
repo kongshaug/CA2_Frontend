c63363b40206e8f97c5a5a2d9c435c0d850e20ef
kongshaug <benja_kongs@hotmail.com>
2019-10-13 03:52:36 +0200

    front

--- src/index.js
+++ src/index.js
@@ -1,151 +1,180 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
+
+populateTable("all");
+document.getElementById("phone").onclick = function(){getPersonByNumber( document.getElementById("phoneNumber").value) }
+document.getElementById("hobby").onclick = function(){populateTable("hobby/" + document.getElementById("hobbyName").value) }
+document.getElementById("city").onclick = function(){ populateTable("city/" + document.getElementById("CityName").value) }
+document.getElementById("countHobby").onclick = countPersonsWithHoppy;
+document.getElementById("zip").onclick = GetAllZip;
+document.getElementById("addPerson").onclick = addPerson;
+
 function populateTable(endpoint){
 fetch("https://www.ajuhlhansen.dk/CA2/api/person/"+ endpoint)
-            .then(res => res.json()) //in flow1, just do it
+            .then(handleHttpErrors) //in flow1, just do it
             .then(data => {
                 // Inside this callback, and only here, the response data is available
                 var allPersons = data.map(person => "<tr><td>" + person.firstname
                 + "</td><td>" + person.lastname + "</td>"
-                + "<td>" + person.email + 
+                + "<td>" + person.email 
                 + "</td><td>"+ person.street + " "+ person.addInfo   
                 + "</td><td>" + person.zip + " " + person.city 
-                + "</td><td>" + person.hobbies.hobby.join(", ")
-                + "</td><td>" + person.phones.phone.join(", ") 
-                + "</td><td>" + '<button id = "'+person.firstname+ ', value="remove" type="button">Remove</button>'
-                + "</td><td>" + '<button id = "'+person.firstname+ ', value="edit"" type="button">Edit</button>'
+                + "</td><td>" + person.hobbies.map(hobby => hobby.hobby).join(", ")
+                + "</td><td>" + person.phones.map(phone => phone.phone).join(", ") 
+                + "</td><td>" + '<button id = '+person.id+ ' value="remove" type="button">Remove</button>'
                 + "</td></tr>");
 
 
                 document.getElementById("tablebody").innerHTML = allPersons.join("");
 
+            })
+            .catch(err => {
+                if(err.status)
+                    err.fullError.then(e => document.getElementById("error").innerHTML = e.message);
+                else document.getElementById("error").innerHTML = "Network error";
             });
+}
+
+function HideOrShow(idOfElement) {
+        var x = document.getElementById(idOfElement);
+        if (x.style.display === "none") {
+            x.style.display = "";
+        } else {
+             x.style.display = "none";
         }
-            function HideOrShow(idOfElement) {
-                var x = document.getElementById(idOfElement);
-                if (x.style.display === "none") {
-                  x.style.display = "";
-                } else {
-                  x.style.display = "none";
-                }
-              }
-              HideOrShow("allZipcodes")      
+        }
+        HideOrShow("allZipcodes")      
 
-            document.getElementById("table").onclick = function(){ EditOrDelete(event.srcElement.id, event.srcElement.value )}
+        document.getElementById("table").onclick = function(){ deletePerson(event.srcElement.id)
+}
 
-            function EditOrDelete(id, value){
-               if(value === "remove")
-               {
-                removePerson(id);
-                populateTable("all");
 
-               }
-               
-                else if(value === "edit")
-               {
-                editPerson(id);
-               }
-               
-
-            }
-
-            function removePerson(id){
-
-                    return fetch("www.ajuhlhansen.dk/CA2/api/person/" + id, {
-                      method: 'delete'
-                    })
-                    .then(response => response.json());
-                  }
+function deletePerson(id) 
+{
+    
+    const options = makeOptions("DELETE");
+    fetch("https://www.ajuhlhansen.dk/CA2/api/person/" + id,options)
+    .then(handleHttpErrors)
+    .then(function(){populateTable("all")})
+    .catch(err => {
+        if(err.status)
+        err.fullError.then(e => document.getElementById("error").innerHTML = e.message);
+        else document.getElementById("error").innerHTML = "Network error";
+    });
+}
 
             
-            function editPerson(id){
+function editPerson(id){
 
-            }
+}
 
-            populateTable("all");
+function getPersonByNumber(number){
 
 
-            document.getElementById("phone").onclick = function(){populateTable("REST ENDPOINT NAME" + document.getElementById("phoneNumber").value) }
-            document.getElementById("hobby").onclick = function(){populateTable("REST ENDPOINT NAME" + document.getElementById("hobbyName").value) }
-            document.getElementById("city").onclick = function(){ populateTable("REST ENDPOINT NAME" + document.getElementById("CityName").value) }
-            document.getElementById("countHobby").onclick = function(){ populateTable("REST ENDPOINT NAME" + document.getElementById("hobbyName").value) }
+        fetch("https://www.ajuhlhansen.dk/CA2/api/person/"+number)
+        .then(handleHttpErrors)
+        .then(data => {
+         // Inside this callback, and only here, the response data is available
+             var person = "<tr><td>" + data.firstname
+            + "</td><td>" + data.lastname + "</td>"
+            + "<td>" + data.email 
+            + "</td><td>"+ data.street + " "+ data.addInfo   
+            + "</td><td>" + data.zip + " " + data.city 
+            + "</td><td>" + data.hobbies.map(hobby => hobby.hobby).join(", ")
+            + "</td><td>" + data.phones.map(phone => phone.phone).join(", ") 
+            + "</td><td>" + '<button id = "'+data.firstname+ ', value="remove" type="button">Remove</button>'
+            + "</td><td>" + '<button id = "'+data.firstname+ ', value="edit"" type="button">Edit</button>'
+            + "</td></tr>";
+            
+            document.getElementById("tablebody").innerHTML = person;
+    
+            })
+            .catch(err => {
+                if(err.status)
+                    err.fullError.then(e => document.getElementById("error").innerHTML = e.message);
+                else document.getElementById("error").innerHTML = "Network error";
+            });
+}
 
-            function countPersonsWithHoppy(){
-                var hobby = document.getElementById("hobbyName");
-                fetch("https://www.ajuhlhansen.dk/CA2/api/person/count/f")
-                .then(res => res.json()) //in flow1, just do it
+          
+
+function countPersonsWithHoppy(){
+        var hobby = document.getElementById("hobbyName").value;
+
+        fetch("https://www.ajuhlhansen.dk/CA2/api/person/count/"+ hobby)
+                .then(handleHttpErrors)
                 .then(data => {
-    
-    
-                    document.getElementById("countDiv").innerHTML = "the number of people with the given hobby is: "+data.count;
 
-                    
+        document.getElementById("countDiv").innerHTML = "the number of people with the given hobby is: "+data.count;
+         })
+         .catch(err => {
+            if(err.status)
+                err.fullError.then(e => document.getElementById("error").innerHTML = e.message);
+            else document.getElementById("error").innerHTML = "Network error";
+        });
 
-                    
-    
-                });
-            }
+}
             
 
-            document.getElementById("zip").onclick = GetAllZip;
-
-            function GetAllZip(){
-                HideOrShow("allZipcodes")
+function GetAllZip(){
+        
+        HideOrShow("allZipcodes")
                 
-                fetch("www.ajuhlhansen.dk/CA2/api/person/zipcodes")
-                .then(res => res.json())
-                .then(data => {
-                    var listofZips = data.map(zip => "<li>" + zip + "</li>")
+            fetch("https://www.ajuhlhansen.dk/CA2/api/person/zipcodes")
+            .then(handleHttpErrors)
+            .then(data => {
+                var listofZips = data.zipcodes.map(zip =>  zip )
 
-                    document.getElementById("zipcode").innerHTML = listofZips.join("");
+                document.getElementById("zipcodes").innerHTML = listofZips.join(", ");
 
-                });
+            })
+            .catch(err => {
+                if(err.status)
+                    err.fullError.then(e => document.getElementById("error").innerHTML = e.message);
+                else document.getElementById("error").innerHTML = "Network error";
+            });
+}
 
-            }
-            document.getElementById("addPerson").onclick = addPerson;
-            function addPerson(){
+           
+function addPerson(){
 
-               var firstName =  document.getElementById("firstName").value
-               var LastName =  document.getElementById("LastName").value
-               var email =  document.getElementById("email").value
-               var city =  document.getElementById("city").value
-               var zip =  document.getElementById("zip").value
-               var street =  document.getElementById("street").value
-               var addInfo =  document.getElementById("addInfo").value
-               var payload = '{\"firstname\": \"'+firstName+'\",'
-               + '\"lastname\": \"'+LastName+'\",'
-               + '\"email\": \"'+email+'\",'
-               + '\"street\": \"'+street+'\",'
-               + '\"addInfo\": \"'+addInfo+'\",'
-               + '\"city\": \"'+city+'\",'
-               + '\"zip\": \"'+zip+'\"}';
+            var firstName =  document.getElementById("firstName").value;
+            var LastName =  document.getElementById("lastName").value;
+            var personEmail =  document.getElementById("email").value;
+            var personCity =  document.getElementById("cityInfo").value;
+            var personzip =  document.getElementById("zipcode").value;
+            var personstreet =  document.getElementById("street").value;
+            var personaddInfo =  document.getElementById("addInfo").value;
+            
+               
+            const data = {firstname: firstName, lastname: LastName, email: personEmail, street: personstreet, addInfo: personaddInfo, city: personCity, zip: personzip};
+            const options = makeOptions("POST",data);
+            fetch("https://www.ajuhlhansen.dk/CA2/api/person/",options)
+            .then(handleHttpErrors)
+            .then(function(){populateTable("all")})
+            .catch(err => {
+                if(err.status)
+                    err.fullError.then(e => document.getElementById("error").innerHTML = e.message);
+                else document.getElementById("error").innerHTML = "Network error";
+            });
+    
+}
 
 
+function handleHttpErrors(res){
+        if(!res.ok)
+        return Promise.reject({status: res.status, fullError: res.json() });
+        return res.json();
+}
 
-          //     try {
-          //    const data = await postData('https://www.ajuhlhansen.dk/CA2/api/person', payload);
-          //      console.log(JSON.stringify(data)); // JSON-string from `response.json()` call
-          //    } catch (error) {
-          //      console.error(error);
-          //    }
-          //    
-          //    async function postData(url = '', data = {}) {
-          //      // Default options are marked with *
-          //      const response = await fetch(url, {
-          //        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-          //        mode: 'cors', // no-cors, *cors, same-origin
-          //        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          //        credentials: 'same-origin', // include, *same-origin, omit
-          //        headers: {
-          //          'Content-Type': 'application/json'
-          //          // 'Content-Type': 'application/x-www-form-urlencoded',
-          //        },
-          //        redirect: 'follow', // manual, *follow, error
-          //        referrer: 'no-referrer', // no-referrer, *client
-          //        body: JSON.stringify(data) // body data type must match "Content-Type" header
-          //      });
-          //      return await response.json(); // parses JSON response into native JavaScript objects
-          //    }
-          //    populateTable("all");
-          //
-             }
+function makeOptions(method, body) {
+        var opts =  {
+        method: method,
+        headers: {"Content-type": "application/json"}
+        }
+        if(body) opts.body = JSON.stringify(body);
+        return opts;
+}
+
+          
+                  
